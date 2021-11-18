bfb762cb3d422bec2133f586fb91f056c0ef755a
Molly Joyce <mjoyce8695@student.pps.net>
2021-11-18 13:50:29 -0800

    11/18

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -14,50 +14,60 @@ public class AgentScript : MonoBehaviour
     public Transform Dest3;
     public Transform Dest4;
 
-    int wait = 0;
+    public int wait = 0;
 
 
     
-//TESTTTTTT
+
+
    
+   void Start(){
 
-    void Start(){
+        
+        agent = this.GetComponent<NavMeshAgent>();
 
-        if(wait = 5){
+        Instantiate(agent); 
+
+        int AssignNum = UnityEngine.Random.Range(0,100);
+
+        if(agent.gameObject.CompareTag ("Type1")){
+             agent.SetDestination(Dest4.position);
+    
+
+        if(agent.gameObject.CompareTag ("Type2")){
+            
+             agent.SetDestination(Dest3.position);
+        }
+        }
+   }
+   
+    
+    
+   /* 
+    void Update {
+
+        if(wait == 5) {
             agent = this.GetComponent<NavMeshAgent>();
 
         Instantiate(agent); 
 
-        int AssignNum = UnityEngine.Random.Range(0f,100f);
+        int AssignNum = UnityEngine.Random.Range(1,2);
 
 
         if(agent.gameObject.CompareTag ("Type1")){
-            if(AssignNum < 50){
+            if(AssignNum < 2f){
              agent.SetDestination(Dest4.position);
         } else {
             agent.SetDestination(Dest3.position);
         }
+        wait = 0;
+    } 
+        } else {
+            wait++;
+        }
     }
-        }
-        //agent = this.GetComponent<NavMeshAgent>();
-
-       // Instantiate(agent); 
-
-        //int AssignNum = UnityEngine.Random.Range(0,100);
-
-        //if(agent.gameObject.CompareTag ("Type1")){
-             //agent.SetDestination(Dest4.position);
     
-
-        //if(agent.gameObject.CompareTag ("Type1")){
-            
-            // agent.SetDestination(Dest4.position);
-        }
-    
-    
-    
-    
-        
+       */ 
        
     
     }
