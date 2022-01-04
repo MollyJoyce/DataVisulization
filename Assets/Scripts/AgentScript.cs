97f9122bb8c52795bc0c80a1cf524c10e448c53a
Molly Joyce <mjoyce8695@student.pps.net>
2022-01-04 13:21:42 -0800

    1/4

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -5,21 +5,34 @@ using UnityEngine.AI;
 
 public class AgentScript : MonoBehaviour
 {
-  
+    public NavMeshAgent self;
+
+    public Transform Dest;
+    public Transform Dest1;
+    public Transform Dest2;
+    public Transform Dest3;
+    public Transform Dest4;
+
+    
 
     void OnTriggerEnter(Collider other){
-       // if (other.gameObject.CompareTag ("Destination")){
-             Destroy(gameObject);
-      // }
+       if (other.gameObject.CompareTag ("Destination")){
+             Destroy(gameObject, 3f);
 
-      if (this.gameObject.CompareTag ("X")){
-        DataCount.XCount++;
-        if (this.NavMeshAgent.Destination == SpawnerScript.Dest){
-          DataCount.OXCount++;
+        if (this.gameObject.CompareTag ("X")){
+          DataCount.XCount++;
+
+          if(self.destination == Dest.position){
+            DataCount.OXCount++;
+          }
+       }
+
+       if (this.gameObject.CompareTag ("Z")){
+          DataCount.ZCount++;
         }
-    }
 
       
     }
+}
 }
 
--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -32,30 +32,19 @@ public class SpawnerScript : MonoBehaviour
     public TextMeshProUGUI THX;
     public TextMeshProUGUI FX;
     public TextMeshProUGUI FIX;
-    
 
+    public TextMeshProUGUI OZ;
+    public TextMeshProUGUI TZ;
+    public TextMeshProUGUI THZ;
+    public TextMeshProUGUI FZ;
+    public TextMeshProUGUI FIZ;
 
-
-
-    
-
-
-    
-
+   
 
    
     void Start(){
 
         
-        
-       
-       
-        
-
-
-
-
-
 
        for (int i = 0; i < total; i++){
 
@@ -70,16 +59,21 @@ public class SpawnerScript : MonoBehaviour
                 ag.GetComponent<Renderer>().material = TOneMat;
                 
                 float destnum = Random.Range(1f, 100f);
-                 if (destnum <= 20){
+                 if (destnum <= 9){
                      ag.SetDestination(Dest.position);
-                 } else if (destnum <= 40){
+                     DataCount.OZCount++;
+                 } else if (destnum <= 46){
                      ag.SetDestination(Dest1.position);
-                 } else if (destnum <= 60){
+                     DataCount.TZCount++;
+                 } else if (destnum <= 75){
                      ag.SetDestination(Dest2.position);
-                 } else if (destnum <= 80){
+                     DataCount.THZCount++;
+                 } else if (destnum <= 91){
                      ag.SetDestination(Dest3.position);
+                     DataCount.FZCount++;
                  } else if (destnum <= 100){
                      ag.SetDestination(Dest4.position);
+                     DataCount.FIZCount++;
                  }
                  //Destroy(ag);
                 // ag.GetComponent<Renderer>().enabled = false;
@@ -89,17 +83,21 @@ public class SpawnerScript : MonoBehaviour
                 ag.GetComponent<Renderer>().material = TTwoMat;
                 
                 float destnum = Random.Range(1f, 100f);
-                 if (destnum <= 20){
+                 if (destnum <= 32){
                      ag.SetDestination(Dest.position);
-                     //DataCount.OXCount++;
-                 } else if (destnum <= 40){
+                     DataCount.OXCount++;
+                 } else if (destnum <= 61){
                      ag.SetDestination(Dest1.position);
-                 } else if (destnum <= 60){
+                     DataCount.TXCount++;
+                 } else if (destnum <= 85){
                      ag.SetDestination(Dest2.position);
-                 } else if (destnum <= 80){
+                     DataCount.THXCount++;
+                 } else if (destnum <= 96){
                      ag.SetDestination(Dest3.position);
+                     DataCount.FXCount++;
                  } else if (destnum <= 100){
                      ag.SetDestination(Dest4.position);
+                     DataCount.FIXCount++;
                  }
             }                
         }
@@ -121,10 +119,18 @@ public class SpawnerScript : MonoBehaviour
 
 
     void Update () {
-        OX.text = Mathf.Round((DataCount.OXCount / total)*100).ToString() + "% X";
-        THX.text = Mathf.Round((DataCount.THXCount / total)*100).ToString() + "% X";
-        FX.text = Mathf.Round((DataCount.FXCount / total)*100).ToString() + "% X";
-        FIX.text = Mathf.Round((DataCount.FIXCount / total)*100).ToString() + "% X";
+        OX.text = Mathf.Round((DataCount.OXCount / total)*100).ToString() + "% R";
+        TX.text = Mathf.Round((DataCount.TXCount / total)*100).ToString() + "% R";
+        THX.text = Mathf.Round((DataCount.THXCount / total)*100).ToString() + "% R";
+        FX.text = Mathf.Round((DataCount.FXCount / total)*100).ToString() + "% R";
+        FIX.text = Mathf.Round((DataCount.FIXCount / total)*100).ToString() + "% R";
+
+        OZ.text = Mathf.Round((DataCount.OZCount / total)*100).ToString() + "% P";
+        TZ.text = Mathf.Round((DataCount.TZCount / total)*100).ToString() + "% P";
+        THZ.text = Mathf.Round((DataCount.THZCount / total)*100).ToString() + "% P";
+        FZ.text = Mathf.Round((DataCount.FZCount / total)*100).ToString() + "% P";
+        FIZ.text = Mathf.Round((DataCount.FIZCount / total)*100).ToString() + "% P";
+
 
 }
 
